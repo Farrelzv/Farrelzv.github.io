95bc5cf48440d7ca3192928ee44dbcbf06443538
FarrelDanCaca <fzulviano@gmail.com>
2022-07-15 13:14:29 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const txtElemet = ['Backend Developer',];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+    if(count == txtElemet.length){
+        count = 0;
+    }
+
+    currentTxt = txtElemet[count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-ngetik').textContent = words;
+
+    if(words.length == currentTxt.length) {
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(ngetik, 300);
+    
+
+})();
